e58d6b347433ecde1b44536cccf60ae1a57436b1
Colin Cavanaugh <colin.cavanaugh35@gmail.com>
2022-02-04 12:07:16 -0500

    add user-invitation

---
+++ api/components/user-invitation/model.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+import { UtilityService } from '../../services/utility';
+
+@Entity()
+export class UserInvitation {
+  constructor(id: number, email: string, hash: string, active: boolean) {
+    this.id = id;
+    this.email = email;
+    this.uuid = hash;
+    this.active = active;
+  }
+
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column({
+    nullable: false,
+    unique: true
+  })
+  public email: string;
+
+  @Column({
+    nullable: false,
+    unique: true
+  })
+  public uuid: string;
+
+  @Column({
+    default: true
+  })
+  public active: boolean;
+
+  public static mockTestUserInvitation(): UserInvitation {
+    return new UserInvitation(1, 'test@email.com', UtilityService.generateUuid(), true);
+  }
+}
---
+++ api/components/user-invitation/repository.ts
@@ -0,0 +1,11 @@
+import { getManager } from 'typeorm';
+
+import { AbsRepository } from '../helper';
+
+import { UserInvitation } from './model';
+
+export class UserInvitationRepository extends AbsRepository<UserInvitation> {
+  constructor() {
+    super('user-invitation', getManager().getRepository(UserInvitation));
+  }
+}
